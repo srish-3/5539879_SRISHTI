100f899b2bb8c6a4ee06dc09bd183f8e4c8239de
Srishtidbadami <srishtidbadami@gmail.com>
2025-08-12 13:59:15 +0530

    added bomberman game sol week 2

---
+++ Hackerrank/week 3/week 3/The bomberman game.c
@@ -0,0 +1,81 @@
+ char** copyGrid(char** grid, int rows, int cols) {
+    char** newGrid = malloc(rows * sizeof(char*));
+    for (int i = 0; i < rows; i++) {
+        newGrid[i] = malloc((cols + 1) * sizeof(char));
+        strcpy(newGrid[i], grid[i]);
+    }
+    return newGrid;
+}
+
+char** detonate(char** grid, int rows, int cols) {
+    char** result = malloc(rows * sizeof(char*));
+    for (int i = 0; i < rows; i++) {
+        result[i] = malloc((cols + 1) * sizeof(char));
+        for (int j = 0; j < cols; j++) {
+            result[i][j] = 'O';
+        }
+        result[i][cols] = '\0';
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (grid[i][j] == 'O') {
+                result[i][j] = '.';
+                if (i > 0) result[i - 1][j] = '.';
+                if (i < rows - 1) result[i + 1][j] = '.';
+                if (j > 0) result[i][j - 1] = '.';
+                if (j < cols - 1) result[i][j + 1] = '.';
+            }
+        }
+    }
+
+    return result;
+} char** copyGrid(char** grid, int rows, int cols) {
+    char** newGrid = malloc(rows * sizeof(char*));
+    for (int i = 0; i < rows; i++) {
+        newGrid[i] = malloc((cols + 1) * sizeof(char));
+        strcpy(newGrid[i], grid[i]);
+    }
+    return newGrid;
+}
+
+char** detonate(char** grid, int rows, int cols) {
+    char** result = malloc(rows * sizeof(char*));
+    for (int i = 0; i < rows; i++) {
+        result[i] = malloc((cols + 1) * sizeof(char));
+        for (int j = 0; j < cols; j++) {
+            result[i][j] = 'O';
+        }
+        result[i][cols] = '\0';
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (grid[i][j] == 'O') {
+                result[i][j] = '.';
+                if (i > 0) result[i - 1][j] = '.';
+                if (i < rows - 1) result[i + 1][j] = '.';
+                if (j > 0) result[i][j - 1] = '.';
+                if (j < cols - 1) result[i][j + 1] = '.';
+            }
+        }
+    }
+
+    return result;
+}
+char** bomberMan(int n, int r, int c, char** grid, int grid_rows, int grid_columns) {
+    if (n == 1) return copyGrid(grid, r, c);
+
+    if (n % 2 == 0) {
+        char** fullGrid = malloc(r * sizeof(char*));
+        for (int i = 0; i < r; i++) {
+            fullGrid[i] = malloc((c + 1) * sizeof(char));
+            for (int j = 0; j < c; j++) {
+                fullGrid[i][j] = 'O';
+            }
+            fullGrid[i][c] = '\0';
+        }
+        return fullGrid;
+    }
+
+
